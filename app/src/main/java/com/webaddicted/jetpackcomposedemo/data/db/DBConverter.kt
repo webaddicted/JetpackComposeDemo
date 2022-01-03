5349d6737513bac8f17a85daf747cd21a6e91f81
Deepak Sharma <deepaksharma040695@gmail.com>
2022-01-03 14:36:47 +0530

    Change theme

---
+++ app/src/main/java/com/webaddicted/jetpackcomposedemo/data/db/DBConverter.kt
@@ -0,0 +1,35 @@
+package com.webaddicted.jetpackcomposedemo.data.db
+
+object DBConverter {
+//    fun beatToUiTypeRespo(data: ArrayList<TodayBeatEntity>): ApiResponse<CommonListRespo<BeatRespo>> {
+//        val beatRespo: ArrayList<BeatRespo> = arrayListOf()
+//        for (index in 0 until data.size) {
+//            val entity = data[index]
+//            beatRespo.add(
+//                BeatRespo(
+//                    entity.badgeIcon,
+//                    entity.beatStatus,
+//                    entity.comment,
+//                )
+//            )
+//        }
+//        return ApiResponse.success(CommonListRespo(true, beatRespo,""))
+//    }
+//
+//    fun beatToDbTypeRespo(todayBeatData: ArrayList<BeatRespo>): ArrayList<TodayBeatEntity> {
+//        val beatDBRespo: ArrayList<TodayBeatEntity> = arrayListOf()
+//        for (index in 0 until todayBeatData.size) {
+//            val entity = todayBeatData[index]
+//            beatDBRespo.add(
+//                TodayBeatEntity(
+//                    (index+1).toLong(),
+//                    entity.badgeIcon,
+//                    entity.beatStatus,
+//                    entity.comment,
+//                    entity.creditDays,
+//                )
+//            )
+//        }
+//        return beatDBRespo
+//    }
+}
