c6dad4d68d66e57a4c30f2778d3fbac975dd630e
Deepak Sharma <deepaksharma040695@gmail.com>
2022-01-03 13:38:05 +0530

    Change theme

--- app/src/main/java/com/webaddicted/jetpackcomposedemo/ui/theme/Color.kt
+++ app/src/main/java/com/webaddicted/jetpackcomposedemo/ui/theme/Color.kt
@@ -6,3 +6,11 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val Green200 = Color(60, 210, 113)
+val Green700 = Color(60, 179, 113)
+val Green500 = Color(60, 144, 113)
+//val Purple700 = Color(0xFF3700B3)
+//val Teal200 = Color(0xFF03DAC5)
+//
+//
+//3CB371
