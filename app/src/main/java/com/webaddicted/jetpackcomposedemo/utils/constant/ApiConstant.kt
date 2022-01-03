ac8b7a2cad4871d74034fd85d6ac4fa994ecc26f
Deepak Sharma <deepaksharma040695@gmail.com>
2022-01-03 14:36:53 +0530

    Change theme

---
+++ app/src/main/java/com/webaddicted/jetpackcomposedemo/utils/constant/ApiConstant.kt
@@ -0,0 +1,7 @@
+package com.webaddicted.jetpackcomposedemo.utils.constant
+
+object ApiConstant {
+    const val DB_NAME = "UserInfo.db"
+    const val USER_INFO_TABLE = "User"
+    const val DB_VERSION = 1
+}
